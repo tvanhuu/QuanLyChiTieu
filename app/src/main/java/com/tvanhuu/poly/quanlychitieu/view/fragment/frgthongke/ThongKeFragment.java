20558cc5e7ab40e928d667b0c8f57e20ec9a2a9e
tvanhuu <ithuutran@gmail.com>
2018-03-08 10:12:12 +0700

    update

--- app/src/main/java/com/tvanhuu/poly/quanlychitieu/view/fragment/frgthongke/ThongKeFragment.java
+++ app/src/main/java/com/tvanhuu/poly/quanlychitieu/view/fragment/frgthongke/ThongKeFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatTextView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,7 +44,7 @@ public class ThongKeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceStte) {
-        return inflater.inflate(R.layout.frge_thongke, container, false);
+        return inflater.inflate(R.layout.frg_thongke, container, false);
     }
 
     @Override
